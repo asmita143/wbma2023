6818a9724fe7c9ebd30d81e7264ea3ee478dce20
asmita143 <89544722+asmita143@users.noreply.github.com>
2023-01-09 19:35:21 +0200

    Initialized and first app work done till

---
+++ App.js
@@ -0,0 +1,25 @@
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, SafeAreaView} from 'react-native';
+import List from './components/List'
+
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <List />
+      </SafeAreaView>
+      <StatusBar style="auto"/>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+export default App;
---
+++ components/ListItems.js
@@ -0,0 +1,27 @@
+import {StatusBar} from 'expo-status-bar';
+import PropTypes from 'prop-types';
+import { Image, StyleSheet, TouchableOpacity, View,Text,} from 'react-native';
+
+
+const ListItem=({singleMedia})=>{
+  const item = singleMedia;
+  return(
+  <TouchableOpacity>
+    <Image
+      style={{width: 100, height: 100}}
+      source={{uri: item.thumbnails.w160}}
+    ></Image>
+  <View >
+    <Text>{item.title}</Text>
+    <Text>{item.description}</Text>
+  </View>
+</TouchableOpacity>
+  );
+};
+ListItem.propTypes={
+  singleMedia: PropTypes.object,
+};
+export default ListItem;
+
+
+
