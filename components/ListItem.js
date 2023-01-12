553146f9b47a7610dad98ef27c788fc36f1efaac
asmita143 <89544722+asmita143@users.noreply.github.com>
2023-01-12 10:33:24 +0200

    styles added for list item

--- components/ListItem.js
+++ components/ListItem.js
@@ -6,18 +6,40 @@ import {uploadsUrl} from '../utils/variables';
 const ListItem=({singleMedia})=>{
   const item = singleMedia;
   return(
-  <TouchableOpacity>
+  <TouchableOpacity style={styles.row}>
+    <View style={styles.box}>
     <Image
-      style={{width: 100, height: 100}}
+      style={styles.image}
       source={{uri: uploadsUrl + item.thumbnails?.w160}}
     ></Image>
-  <View >
-    <Text>{item.title}</Text>
+    </View>
+  <View style={styles.box}>
+  <Text style={styles.listTitle}>{item.title}</Text>
     <Text>{item.description}</Text>
   </View>
 </TouchableOpacity>
   );
 };
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    backgroundColor: '#ccc',
+    marginBottom: 10,
+  },
+  box: {
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    flex: 1,
+    minHeight: 100,
+  },
+  listTitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingBottom: 15,
+  },
+});
 ListItem.propTypes={
   singleMedia: PropTypes.object,
 };
