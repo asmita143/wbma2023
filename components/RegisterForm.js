99ce48d6d48105d36c1c8745ad17e08bb9782131
asmita143 <89544722+asmita143@users.noreply.github.com>
2023-01-25 12:03:07 +0200

    form validation

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {useUser} from '../hooks/ApiHooks';
 import { Button, TextInput, Text, View} from 'react-native';
 import {Controller, useForm} from 'react-hook-form';
+import {Pattern} from 'react-native-svg';
+
 
 const RegisterForm=()=>{
   //const {setIsLoggedIn}=useContext(MainContext);
   //const{postLogin}=useAuthentication();
-  const {postUser}=useUser();
+  const {postUser,checkUserName}=useUser();
   const{control,handleSubmit,formState:{errors}}=useForm({
     defaultValues:{username: '',password:'',email:'',full_name:''},
+    mode:'onBlur',
   });
 
   const register = async(registerData) => {
@@ -22,12 +25,24 @@ const RegisterForm=()=>{
       console.error('register', error);
     }
   };
+  const checkUser = async (username) => {
+    try {
+      const userAvailable = await checkUserName(username);
+    console.log('checkUser', userAvailable);
+    return userAvailable || 'Username is already taken';
+    } catch (error) {
+      console.error('checkUser',error.message)
+    }
+
+  };
     return (
       <View >
         <Text>Register Form</Text>
         <Controller
         control={control}
-        rules={{required:true, minLength:3}}
+        rules={{required:{value:true,message:'This is required'},
+        minLength:{value:34,message:'Username min length is 3 character'},
+        validate: checkUser}}
         render={(
           { field:{onChange,onBlur,value}}
         )=>(
@@ -36,15 +51,24 @@ const RegisterForm=()=>{
           onBlur={onBlur}
           onChangeText={onChange}
           value={value}
+          autoCapitalize='none'
+          errorMessage={errors.username && errors.username.message}
           />
         )}
         name="username"
         />
         {errors.username?.type === 'required' && <Text>is required</Text>}
         {errors.username?.type === 'minLength' && <Text>min length is 3 characters</Text>}
+
         <Controller
         control={control}
-        rules={{required:true, minLength:5}}
+        rules={{
+          required:{value:true, message:'min 5 characters, needs 1 number & 1 uppercase letter'},
+          pattern :{
+            value:/(?=.*\p{Lu})(?=.*[0-9]).{5,}/u,
+            message:'min 5 characters, needs 1 number & 1 uppercase letter'},
+          }
+        }
         render={(
           { field:{onChange,onBlur,value}}
         )=>(
@@ -54,6 +78,8 @@ const RegisterForm=()=>{
           onChangeText={onChange}
           value={value}
           secureTextEntry={true}
+          autoCapitalize='none'
+          errorMessage={errors.password && errors.password.message}
           />
         )}
         name="password"
@@ -97,7 +123,5 @@ const RegisterForm=()=>{
     );
 };
 
-
-RegisterForm.propTypes={};
 
 export default RegisterForm;
--- views/Login.js
+++ views/Login.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {StyleSheet, Keyboard, KeyboardAvoidingView, TouchableOpacity, View, Platform} from 'react-native';
 import PropTypes from 'prop-types';
 import {MainContext} from '../contexts/MainContext';
@@ -6,10 +6,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
 import { useUser } from '../hooks/ApiHooks';
+import {Button, Text} from '@rneui/base'
 
 const Login = ({navigation}) => {
   const {setIsLoggedIn,setUser}=useContext(MainContext);
   const {getUserByToken} = useUser();
+
+  const [toggleForm, setToggleForm] = useState(true);
 
   const checkToken=async()=>{
     try{
@@ -34,8 +37,19 @@ const Login = ({navigation}) => {
     <KeyboardAvoidingView
     behaviour={Platform.OS === 'ios'? 'padding' : 'height'}
      style={styles.container}>
-      <LoginForm />
-      <RegisterForm />
+       {toggleForm ? <LoginForm /> : <RegisterForm />}
+        <Text>
+          {toggleForm
+            ? 'No account yet? Please register.'
+            : 'Already have an accoung? Please login.'}
+        </Text>
+        <Button
+          type="outline"
+          title={toggleForm ? 'Go to register' : 'Go to login'}
+          onPress={() => {
+            setToggleForm(!toggleForm);
+          }}
+        />
     </KeyboardAvoidingView>
     </TouchableOpacity>
   );
